ad7bd46d78bfd2b3b001db3a1973d49e0f5d626a
Ayin Kim <starmireu@gmail.com>
2024-07-24 12:47:50 +0900

    initial commit

---
+++ src/test/java/com/fls/animecommunity/animesanctuary/AnimeSanctuaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fls.animecommunity.animesanctuary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeSanctuaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
