3347d5c613dd388d2b89f5d2b116e1a90e88a26e
Ayin Kim <starmireu@gmail.com>
2024-08-07 12:19:22 +0900

    feat - member

---
+++ src/main/java/com/fls/animecommunity/animesanctuary/model/member/dto/MemberResponseDto.java
@@ -0,0 +1,32 @@
+package com.fls.animecommunity.animesanctuary.dto;
+
+public class MemberResponseDto {
+    private Long id;
+    private String username;
+    private String email;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
