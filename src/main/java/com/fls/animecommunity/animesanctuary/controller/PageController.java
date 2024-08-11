60df7229b3bbaa17ed4d570c65e445b20cba393d
taharma <163256001+taharma@users.noreply.github.com>
2024-08-11 13:12:22 +0900

    Update read.html
    clientside에서 하는식으로 일단 구현

--- src/main/java/com/fls/animecommunity/animesanctuary/controller/PageController.java
+++ src/main/java/com/fls/animecommunity/animesanctuary/controller/PageController.java
@@ -1,17 +1,27 @@
 package com.fls.animecommunity.animesanctuary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fls.animecommunity.animesanctuary.model.post.dto.PostResponseDto;
+import com.fls.animecommunity.animesanctuary.service.PostService;
 
 
 @Slf4j
 @Controller
 @RequestMapping("board")
+@RequiredArgsConstructor
 public class PageController {
+	
+	private final PostService postService;
+	
 	
 	//list
 	@GetMapping("list")
@@ -20,10 +30,13 @@ public class PageController {
 	}
 	
 	//read
-	@GetMapping("read")
-	public String read() {
-		return "read";
+	@GetMapping("read/{id}")
+	public String getPost(@PathVariable("id") Long id, Model model) {
+	    PostResponseDto post = postService.getPost(id);
+	    model.addAttribute("post", post);
+	    return "read"; // "read.html" 템플릿 반환
 	}
+
 	
 	//write
 	@GetMapping("write")
