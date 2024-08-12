feb11d03616fdb0dab0d530c6796a80f5b59fc96
taharma <163256001+taharma@users.noreply.github.com>
2024-08-12 11:00:07 +0900

    Feat: validation
    validation 구현
    postRequestDto에 @NotBlank
    @validated

--- src/main/java/com/fls/animecommunity/animesanctuary/model/post/dto/PostRequestsDto.java
+++ src/main/java/com/fls/animecommunity/animesanctuary/model/post/dto/PostRequestsDto.java
@@ -1,11 +1,19 @@
 package com.fls.animecommunity.animesanctuary.model.post.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
+
 public class PostRequestsDto {
+	
+	@NotEmpty
 	private String title;
+	@NotEmpty
     private String contents;
+	@NotBlank
     private String author;
+	@NotBlank
     private String password;
 }
