ba10ef739916195c283caf38e42285529338b718
Chesed Kim <starmireu@gmail.com>
2024-08-16 21:33:38 +0900

    Add MemberRegisterDto

---
+++ src/main/java/com/fls/animecommunity/animesanctuary/dto/MemberRegisterDto.java
@@ -0,0 +1,16 @@
+package com.fls.animecommunity.animesanctuary.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter @Setter
+public class MemberRegisterDto {
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String gender;
+    private LocalDate birth;
+}
