3ed91a929394650bb65d07956fee1bfb4ccf5e7c
Ayin Kim <starmireu@gmail.com>
2024-08-21 11:47:37 +0900

    Revert "(Temporary) Revert "Add Swagger (Springdoc)""

--- src/main/java/com/fls/animecommunity/animesanctuary/AnimeSanctuaryApplication.java
+++ src/main/java/com/fls/animecommunity/animesanctuary/AnimeSanctuaryApplication.java
@@ -3,8 +3,11 @@ package com.fls.animecommunity.animesanctuary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Anime Sanctuary API", version = "1.0", description = "各国の主要アニメプラットフォームを一目で見ることができる「Anime Sanctuary」サービスです。"))
 @EnableJpaAuditing
 public class AnimeSanctuaryApplication {
 
