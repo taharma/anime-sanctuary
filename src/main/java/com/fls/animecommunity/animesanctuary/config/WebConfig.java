78d89948617685e0b9d3dc18d961edb653b1e99d
j1nh22 <dd02120915@icloud.com>
2024-08-31 18:40:24 +0900

    fetchtest
    Check with live server

--- src/main/java/com/fls/animecommunity/animesanctuary/config/WebConfig.java
+++ src/main/java/com/fls/animecommunity/animesanctuary/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5501")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
