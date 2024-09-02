1090e0023ab94ea259d11e477c21caff9475dd13
j1nh22 <dd02120915@icloud.com>
2024-09-02 11:46:03 +0900

    Changes_0902
    구글 소셜 로그인 구현 완료,
    localhost:9000 으로 들어가야 확인할 수 있음, 로그인 후 db까지 확인 완료함

--- src/main/java/com/fls/animecommunity/animesanctuary/model/member/GenderType.java
+++ src/main/java/com/fls/animecommunity/animesanctuary/model/member/GenderType.java
@@ -1,19 +1,25 @@
 package com.fls.animecommunity.animesanctuary.model.member;
 
-
-
 public enum GenderType {
-	MALE("남성"),
-	FEMALE("여성");
-	
-	private String description; //필드 선언
-	
-	//생성자. enum의 특징: 생성자의 접근지정자가 private
-	private GenderType(String description) {
-		this.description = description;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
+    MALE("남성"),
+    FEMALE("여성"),
+    OTHER("기타");
+
+    private final String description;
+
+    // 생성자
+    GenderType(String description) {
+        this.description = description;
+    }
+
+    // 설명을 반환하는 메서드
+    public String getDescription() {
+        return description;
+    }
+
+    // 열거형의 문자열 표현을 오버라이드
+    @Override
+    public String toString() {
+        return description;
+    }
 }
