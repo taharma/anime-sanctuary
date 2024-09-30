7a4b7f297065a0dce6238fef3d2c57b5caddc50f
taharma <163256001+taharma@users.noreply.github.com>
2024-09-30 09:36:49 +0900

    feat: Note Regex

--- src/main/java/com/fls/animecommunity/animesanctuary/model/note/dto/NoteRequestsDto.java
+++ src/main/java/com/fls/animecommunity/animesanctuary/model/note/dto/NoteRequestsDto.java
@@ -13,19 +13,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class NoteRequestsDto {
-    @NotBlank(message = "タイトルは必須です。")
-    @Size(min = 5, max = 100, message = "タイトルは5文字以上100文字以内で入力してください。")
+    @NotBlank(message = "제목은 필수 입력값입니다.")
+    @Size(min = 1, max = 100, message = "제목은 1자 100자 이내로 작성해주세요.")
     @Pattern(regexp = "^[a-zA-Z0-9ぁ-んァ-ン一-龥々ー\\s]*$", message = "タイトルには、英数字、ひらがな、カタカナ、漢字、空白のみ使用できます。")
     private String title;
 
-    @NotBlank(message = "内容は必須です。")
-    @Size(min = 10, max = 1000, message = "内容は10文字以上1000文字以内で入力してください。")
+    @NotBlank(message = "내용은 필수 입력값입니다.")
+    @Size(min = 1, max = 10000, message = "内容は1文字以上10000文字以内で入力してください。")
     @Pattern(regexp = "^[a-zA-Z0-9ぁ-んァ-ン一-龥々ー\\s]*$", message = "内容には、英数字、ひらがな、カタカナ、漢字、空白のみ使用できます。")
     private String contents;
 
-    // カテゴリーID (注意: カテゴリー id = 1 anime など)
     private Long categoryId;
 
-    // 作成者ID
     private Long memberId;
 }
